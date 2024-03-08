fefdd996490fa546d6c820dabc82d8a4679a8c22
krentkatarina <krentkatarina@mail.ru>
2024-03-09 01:25:10 +0300

    br2jsx

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import BR2JSX  from './components/br2jsx/br2jsx'
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  let text = "первый<br>второй<br/>третий<br />последний";
+    return (
+        <BR2JSX text={text} />
+    ) 
 }
 
 export default App;
